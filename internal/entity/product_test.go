ef6e7bad99cb18db558464173a60249da09059eb
Lucas Ferreira <casferreiraa@gmail.com>
2023-09-19 21:14:44 -0300

    feat(product): create product entity and test file

---
+++ internal/entity/product_test.go
@@ -0,0 +1,49 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewProduct(t *testing.T) {
+
+	prod, err := NewProduct("Produto 1", 10)
+	assert.Nil(t, err)
+	assert.NotNil(t, prod)
+	assert.NotEmpty(t, prod.ID)
+	assert.Equal(t, "Produto 1", prod.Name)
+	assert.Equal(t, 10, prod.Price)
+
+}
+
+func TestProductWhenNameIsRequired(t *testing.T) {
+
+	prod, err := NewProduct("", 10)
+	assert.Nil(t, prod)
+	assert.Equal(t, ErrNameIsRequired, err)
+
+}
+
+func TestProductWhenPriceIsRequired(t *testing.T) {
+
+	prod, err := NewProduct("Product", 0)
+	assert.Nil(t, prod)
+	assert.Equal(t, ErrPriceIsRequired, err)
+
+}
+
+func TestProductWhenPriceIsInvalid(t *testing.T) {
+
+	prod, err := NewProduct("Product", -10)
+	assert.Nil(t, prod)
+	assert.Equal(t, ErrInvalidPrice, err)
+
+}
+
+func TestProductValidate(t *testing.T) {
+	prod, err := NewProduct("Product 01", 10)
+	assert.Nil(t, err)
+	assert.NotNil(t, prod)
+	assert.Nil(t, prod.Validate())
+}
