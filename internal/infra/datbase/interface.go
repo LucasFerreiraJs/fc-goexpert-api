69e84c1cf80ead1d5cedfcebd0a7f114791652b6
Lucas Ferreira <casferreiraa@gmail.com>
2023-09-21 22:28:42 -0300

    feat(Product): create product crud

--- internal/infra/datbase/interface.go
+++ internal/infra/datbase/interface.go
@@ -7,18 +7,10 @@ type UserInterface interface {
 	FindByEmail(email string) (*entity.User, error)
 }
 
-func (u *User) Create(user *entity.User) error {
-
-	return u.DB.Create(user).Error
-
-}
-
-func (u *User) FindByEmail(email string) (*entity.User, error) {
-
-	var user entity.User
-	err := u.DB.Where("email = ?", email).First(&user).Error
-	if err != nil {
-		return nil, err
-	}
-	return &user, nil
+type ProductInterface interface {
+	Create(product *entity.Product) error
+	FindAll(page, limit int, sort string) ([]entity.Product, error)
+	FindById(id string) (entity.User, error)
+	Update(product *entity.Product) error
+	Delete(id string) error
 }
